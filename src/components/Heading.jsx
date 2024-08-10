a05342918c13ef0700f70833a119ffd877f52e00
Yogesh Palve <dnyyogesh@gmail.com>
2024-08-10 21:06:41 +0530

    Benefits, Notification, Hero, Heading, Generation added

---
+++ src/components/Heading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Heading = ({className, title}) => {
+  return (
+    <div
+     className={`${className} max-w-[50rem]`}
+    >
+        {title && <h2 className='h2'>{title}</h2>}
+    </div>
+  )
+}
+
+export default Heading
